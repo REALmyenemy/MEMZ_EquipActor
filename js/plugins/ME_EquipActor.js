8ecbef4959552865c2f3f0b39db87e18bda428fc
myenemy <26635506+REALmyenemy@users.noreply.github.com>
2025-02-25 09:33:42 +0100

    Drafted the requeriments to update the plugin

--- js/plugins/ME_EquipActor.js
+++ js/plugins/ME_EquipActor.js
@@ -1,7 +1,7 @@
 /*:
 * @target MZ
-* Version 1.1.0
-* Last update 26/07/22
+* Version 1.2.0
+* Last update 05/03/25
 * @author myenemy
 * @plugindesc This plugin allows you to equip items as party members
 * @help
@@ -22,6 +22,25 @@
 * 1.0.0: Actual proper release, stabilized for MZ.
 * 1.0.2: A small bugfix and a revamp of the code convention
 * 1.1.0: A BIG bugfix. Added inheritable skills
+*
+* @param expPercent
+* @text Exp Gain
+* @type number
+* @default 0
+* @desc Set the experience equipedActors earn from basic systems
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
 *
 *
 ==============================================
@@ -50,12 +69,68 @@
 *
 */
 var Imported = Imported || {};
-Imported.ME_EquipActor = "1.1.0";
+Imported.ME_EquipActor = "1.2.0";
 
 function ME_EquipActor() { };
 
 ME_EquipActor.changeEquip = Game_Actor.prototype.changeEquip;
 ME_EquipActor.learnSkill = Game_Actor.prototype.learnSkill;
+ME_EquipActor.levelUp = Game_Actor.prototype.levelUp;
+ME_EquipActor.gainExp = Game_Actor.prototype.gainExp;
+
+/*
+//Actually important function to check if skill is learnt. Add re-checking for inheritable skills here.
+Game_Actor.prototype.levelUp = function() {
+	this._level++;
+	for (const learning of this.currentClass().learnings) {
+		if (learning.level === this._level) {
+			this.learnSkill(learning.skillId);
+		}
+	}
+};
+
+//Function to gain exp and level up, may be helpful
+Game_Actor.prototype.gainExp = function(exp) {
+	const newExp = this.currentExp() + Math.round(exp * this.finalExpRate());
+	this.changeExp(newExp, this.shouldDisplayLevelUp());
+};
+
+//Lead to show a sign for "inherited new skill" in general.
+BattleManager.gainExp = function() {
+	const exp = this._rewards.exp;
+	for (const actor of $gameParty.allMembers()) {
+		actor.gainExp(exp);
+	}
+};
+
+//Lead to show a sign for "inherited new skill", and re-check all inherited skills after battle.
+BattleManager.gainRewards = function() {
+	this.gainExp();
+	this.gainGold();
+	this.gainDropItems();
+};
+
+//To fix optimize, planned to disable items marked as item actors
+Game_Actor.prototype.bestEquipItem = function(slotId) {
+    const etypeId = this.equipSlots()[slotId];
+    const items = $gameParty
+        .equipItems()
+        .filter(item => item.etypeId === etypeId && this.canEquip(item));
+    let bestItem = null;
+    let bestPerformance = -1000;
+    for (let i = 0; i < items.length; i++) {
+        const performance = this.calcEquipItemPerformance(items[i]);
+        if (performance > bestPerformance) {
+            bestPerformance = performance;
+            bestItem = items[i];
+        }
+    }
+    return bestItem;
+};
+
+*/
+Game_Actor.prototype.inheritedSkillSet = null;
+Game_Actor.prototype._naturalSkills = null;
 
 Game_Actor.prototype.changeEquip = function (slot, id) {
 	if (id) {
@@ -68,34 +143,34 @@ Game_Actor.prototype.changeEquip = function (slot, id) {
 	$gamePlayer.refresh()
 };
 
-Game_Actor.prototype.inheritedSkillSet = null;
-Game_Actor.prototype._naturalSkills = null;
 
 ME_EquipActor.isMatch = function (item) {
 	if (item)
 		return item.note.match(/.*<equipactor:\s*([0-9]+)\s*([0-9]*)>.*/i);
 	else return null;
 };
+
 //Checks if actor has the tag for inheritable skill
 ME_EquipActor.hasInheritableSkills = function (actorId) {
 	if (actorId) {
-		var note = $dataActors[actorId].note;
+		let note = $dataActors[actorId].note;
 		if (note)
 			return note.match(/.*<equipactor_inheritableskills:\s*((?:\d+,\s*)*\d+)\s*>.*/i);
 	}
 	return null;
 };
+
 //Checks what skills in the list in the above function, has the actor.
 ME_EquipActor.inheritableSkills = function (actorId) {
-	var inheritableSkills = this.hasInheritableSkills(actorId);
+	let inheritableSkills = this.hasInheritableSkills(actorId);
 
 	if (inheritableSkills) {
 		inheritableSkills = (inheritableSkills[1].replace(" ", "")).split(",");
 
-		var actor = $gameActors.actor(actorId);
-		var list = [];
-		for (var i = 0; i < inheritableSkills.length; i++) {
-			var aux = parseInt(inheritableSkills[i]);
+		let actor = $gameActors.actor(actorId);
+		let list = [];
+		for (let i = 0; i < inheritableSkills.length; i++) {
+			let aux = parseInt(inheritableSkills[i]);
 			if (actor._skills.includes(aux)) {
 				list.push(aux);
 			}
@@ -105,9 +180,10 @@ ME_EquipActor.inheritableSkills = function (actorId) {
 	else
 		return;
 };
+
 //Every time the actor learns a skill naturally, save it into different array, so we can know which ones come from equipment when duped
 Game_Actor.prototype.learnSkill = function (skillId) {
-	ME_EquipActor.learnSkill.call(this, skillId)
+	ME_EquipActor.learnSkill.call(this, skillId) //!!! Get a notification
 	if (this._naturalSkills) {
 		this._naturalSkills.push(skillId);
 	}
@@ -115,15 +191,16 @@ Game_Actor.prototype.learnSkill = function (skillId) {
 		this._naturalSkills = this._skills;
 	}
 };
+
 //Main process
 ME_EquipActor.equipActor = function (slot, id) {
-	var match = ME_EquipActor.isMatch(id);
+	let match = ME_EquipActor.isMatch(id);
 	if (match) {
 		if ($gameParty.members().contains($gameActors.actor(match[1])) && $gameActors.actor(match[1]) != this) {
 			if (match[2]) {
 				$gameSwitches.setValue(match[2], true);
 			}
-			var actorId = parseInt(match[1]);
+			let actorId = parseInt(match[1]);
 			$gameParty.removeActor(actorId);
 
 			ME_EquipActor.changeEquip.call(this, slot, id);
@@ -133,13 +210,14 @@ ME_EquipActor.equipActor = function (slot, id) {
 	else
 		ME_EquipActor.changeEquip.call(this, slot, id);
 };
+
 ME_EquipActor.restoreSkills = function (slot) {
-	var skillsInnherited = this.inheritedSkillSet[slot];
+	let skillsInnherited = this.inheritedSkillSet[slot];
 	this.inheritedSkillSet[slot] = this._naturalSkills;
 
-	for (var i = 0; i < skillsInnherited.length; i++) {
-		var deletable = true;
-		for (var j = 0; j < this.inheritedSkillSet.length && deletable; j++) {
+	for (let i = 0; i < skillsInnherited.length; i++) {
+		let deletable = true;
+		for (let j = 0; j < this.inheritedSkillSet.length && deletable; j++) {
 			if (this.inheritedSkillSet[j] && this.inheritedSkillSet[j].contains(skillsInnherited[i]))
 				deletable = false;
 		}
@@ -150,9 +228,10 @@ ME_EquipActor.restoreSkills = function (slot) {
 	}
 	this.inheritedSkillSet[slot] = null;
 }
-// Removes the actor
+
+// Removes the item actor
 ME_EquipActor.unequipActor = function (slot, id) {
-	var match = ME_EquipActor.isMatch(this.equips()[slot]);
+	let match = ME_EquipActor.isMatch(this.equips()[slot]);
 	if (Array.isArray(match)) {
 		if (match[2]) {
 			$gameSwitches.setValue(match[2], false);
@@ -163,17 +242,18 @@ ME_EquipActor.unequipActor = function (slot, id) {
 	}
 	ME_EquipActor.changeEquip.call(this, slot, id);
 };
+
 // Manages skill inheriting
 ME_EquipActor.learnEquipedSkillset = function (actorId, slot) {
 	if (!this.inheritedSkillSet) {
 		this.inheritedSkillSet = [];
-		for (var i = 0; i < this._equips.length; i++) {
+		for (let i = 0; i < this._equips.length; i++) {
 			this.inheritedSkillSet.push(null);
 		}
 	}
-	var skillset = ME_EquipActor.inheritableSkills(actorId)
+	let skillset = ME_EquipActor.inheritableSkills(actorId)
 	this.inheritedSkillSet[slot] = skillset;
-	for (var i = 0; i < skillset.length; i++) {
+	for (let i = 0; i < skillset.length; i++) {
 		if (!this._skills.contains(skillset[i])) {
 			this._skills.push(skillset[i]);
 		}
